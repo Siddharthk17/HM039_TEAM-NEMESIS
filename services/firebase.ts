c1f376f9cceef00fefd49ede7fd9295abe1bd76c
Siddharthk17 <155727936+Siddharthk17@users.noreply.github.com>
2026-01-21 12:25:58 +0530

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClwUFQKwLn89IUJMEy_XqAw-sZzSgGi7A",
+  apiKey: "",
   authDomain: "nemesis-os.firebaseapp.com",
   projectId: "nemesis-os",
   storageBucket: "nemesis-os.firebasestorage.app",
-  messagingSenderId: "377071190881",
-  appId: "1:377071190881:web:82f63e024c72a597333b20",
-  measurementId: "G-G2ZS058D20"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
